402d14c7fe4247d3d28a43fec45484dbe938bb60
Nopal <hnaufal123@gmail.com>
2025-07-20 07:57:46 +0700

    adjust env

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -21,12 +21,16 @@ pub mod social;
 // }
 
 pub async fn connect() -> mongodb::error::Result<Database> {
-    let mongo_uri = env::var("MONGO_URI").expect("MONGO_URI is not found.");
-    let mongo_db_name = env::var("MONGO_DB_NAME").expect("MONGO_DB_NAME is not found.");
+    let mongo_host = env::var("MONGO_DB_HOST").expect("MONGO_DB_HOST is not found.");
+    let mongo_port = env::var("MONGO_DB_PORT").expect("MONGO_DB_PORT is not found.");
+    let mongo_user = env::var("MONGO_DB_USER").expect("MONGO_DB_USER is not found.");
+    let mongo_pass = env::var("MONGO_DB_PASS").expect("MONGO_DB_PASS is not found.");
+    let mongo_name = env::var("MONGO_DB_NAME").expect("MONGO_DB_NAME is not found.");
+    let mongo_uri = format!("mongodb://{mongo_user}:{mongo_pass}@{mongo_host}:{mongo_port}");
 
     let client_options = ClientOptions::parse(mongo_uri).await?;
     let client = Client::with_options(client_options)?;
-    let database = client.database(mongo_db_name.as_str());
+    let database = client.database(mongo_name.as_str());
 
     println!("MongoDB Connected!");
 
